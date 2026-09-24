e6fc54343dddcf039d8953015ebcd2c65f2f278b
Mitchell Wright <mitchell.wright877@gmail.com>
2026-09-24 19:09:13 +0100

    Requesting gov api and parsing events from html

--- main.go
+++ main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"io"
 	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
 	"time"
 
+	"github.com/PuerkitoBio/goquery"
 	ics "github.com/arran4/golang-ical"
 	"github.com/gin-gonic/gin"
 )
+
+type WasteCollectionEvent struct {
+	Title string
+	Date  time.Time
+}
 
 func main() {
 	router := gin.Default()
@@ -29,4 +39,62 @@ func getCalendar(c *gin.Context) {
 	c.Header("Cache-Control", "no-cache; no-store; must-revalidate")
 
 	c.String(http.StatusOK, calendar.Serialize())
+}
+
+func getWasteCollectionEvents(uprn string, postcode string, month int, year int) ([]WasteCollectionEvent, error) {
+	endpoint := "https://ilambassadorformsprod.azurewebsites.net/wastecollectiondays/wastecollection"
+	data := url.Values{}
+	data.Set("Postcode", postcode)
+	data.Set("Month", strconv.Itoa(month))
+	data.Set("Year", strconv.Itoa(year))
+	data.Set("Uprn", uprn)
+
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", endpoint, strings.NewReader(data.Encode()))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	events, err := parseWasteCollectionEventsFromReader(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return events, nil
+}
+
+func parseWasteCollectionEventsFromReader(reader io.Reader) ([]WasteCollectionEvent, error) {
+	doc, err := goquery.NewDocumentFromReader(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	var events []WasteCollectionEvent
+
+	doc.Find(".cal-month-box .rc-event-container").Each(func(i int, s *goquery.Selection) {
+		eventTitle := strings.TrimSpace(s.Find("span").Text())
+
+		calInnerParent := s.Closest(".cal-inner")
+
+		dateStr, exists := calInnerParent.Find(".day-no").Attr("data-cal-date")
+		parsedTime, err := time.Parse("2006-01-02T15:04:05", dateStr)
+
+		if exists && eventTitle != "" && err == nil {
+			events = append(events, WasteCollectionEvent{
+				Title: eventTitle,
+				Date:  parsedTime,
+			})
+		}
+	})
+
+	return events, nil
 }
