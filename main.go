bad90648c70234b7eedbeda8531fc0ff3a1edc88
Mitchell Wright <mitchell.wright877@gmail.com>
2026-09-25 08:11:37 +0100

    Added missing return statements in error blocks

--- main.go
+++ main.go
@@ -35,12 +35,14 @@ func getCalendarHandler(c *gin.Context) {
 	var query CalendarQuery
 	if err := c.ShouldBindQuery(&query); err != nil {
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
 	}
 
 	currentTime := time.Now()
 	events, err := getWasteCollectionEvents(query.Uprn, query.Postcode, int(currentTime.Month()), currentTime.Year())
 	if err != nil {
 		c.AbortWithError(500, err)
+		return
 	}
 
 	calendar := buildCalendarFromWasteCollectionEvents(events)
